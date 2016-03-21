aecee01ec72af82a90141b3f282adb7468eacd18
Suraj Deshmukh <surajssd009005@gmail.com>
2016-03-21 05:50:08 -0400

    Updated the ATOMICAPP_VERSION in constants file to 0.4.4

--- openshift2nulecule/constants.py
+++ openshift2nulecule/constants.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 HOST_DIR = "/host"
 
-ATOMICAPP_VERSION = "0.4.2"
+ATOMICAPP_VERSION = "0.4.4"
 NULECULE_SPECVERSION = "0.0.2"
 NULECULE_PROVIDERS = ["kubernetes", "openshift"]
