60b9a9948e250c78f807112e92ed8e63fa452c7a
Tomas Kral <tomas.kral@gmail.com>
2016-04-13 17:32:45 +0200

    Update constants.py

--- openshift2nulecule/constants.py
+++ openshift2nulecule/constants.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 HOST_DIR = "/host"
 
-ATOMICAPP_VERSION = "0.4.5"
+ATOMICAPP_VERSION = "0.5.0"
 NULECULE_SPECVERSION = "0.0.2"
 NULECULE_PROVIDERS = ["kubernetes", "openshift"]
